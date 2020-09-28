b548674cd774944b937367f9ae8ce6c348fe4248
Ryan Schniederjan <ryno@schnietke.com>
2020-09-28 15:29:03 -0500

    new student api

--- public/scripts/get.js
+++ public/scripts/get.js
@@ -1,6 +1,12 @@
 function get(url) {
     //Step1: fetch data
-    return fetch(url)
+    return fetch(url,{
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key": "619a10ef17mshd13e544bf683a12p181861jsn82b20c24ddbd"
+        }
+    })
         //Step2: Run the json() method from the server response
         .then(function(response) {
             return response.json();
